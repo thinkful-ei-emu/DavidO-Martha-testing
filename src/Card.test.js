04ba7a3902dc3137038515ec01ef872e762d1cd9
Martha Plantz <maplantz89@gmail.com>
2019-06-25 09:41:47 -0700

    got our smoke screens working!!! yayyy!

--- src/Card.test.js
+++ src/Card.test.js
@@ -6,7 +6,7 @@ describe('<Card />', () =>{
   it('renders without crashing', ()=> {
     const div = document.createElement('div');
   
-    ReactDOM.render(<Card title="Card1" content="Content of card" />, div);
+    ReactDOM.render(<Card key="1" title="Card1" content="Content of card" />, div);
   
     ReactDOM.unmountComponentAtNode(div);
   });
--- src/List.test.js
+++ src/List.test.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import List from './List.js';
-import Card from './Card.js';
+
+const card1 ={
+  title: 'title1',
+  content: 'content of card',
+  key: 1
+}
+const card2 ={
+  title: 'title2',
+  content: 'content of card',
+  key: 2
+}
 
 describe('<List />', () =>{
   it('renders without crashing', ()=> {
     const div = document.createElement('div');
-  
-    ReactDOM.render(<List header="List1" key="1" title="card title" content="content of card" />, div);
-  
+    ReactDOM.render(<List key="1" cards={[card1, card2]} header="header1" />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
