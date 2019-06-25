920c8fbd08c9ad0d708063ac9c61a5721dd08f04
David Omstead <davidomstead@hotmail.com>
2019-06-25 09:44:58 -0700

    not pushing

--- src/List.test.js
+++ src/List.test.js
@@ -7,8 +7,9 @@ describe('<List />', () =>{
   it('renders without crashing', ()=> {
     const div = document.createElement('div');
   
-    ReactDOM.render(<List header="List1" key="1" title="card title" content="content of card" />, div);
+    ReactDOM.render(<List header="List1" card = "SomeNonesense" key="1" title="card title" content="content of card" />, div);
   
     ReactDOM.unmountComponentAtNode(div);
+    console.log("Test this!")
   });
 });
