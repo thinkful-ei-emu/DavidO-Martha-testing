442ef9de8a2441ee95b612bce7a5e1814f4e1e74
Martha Plantz <maplantz89@gmail.com>
2019-06-25 08:40:37 -0700

    pushing what we have having trouble running npm test

---
+++ src/Card.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from '.react-dom';
+import Card from '/.Card';
+
+describe('<Card />', () =>{
+  it('renders without crashing', ()=> {
+    const div = document.createElement('div');
+  
+    ReactDOM.render(<Card title="Card1" content="Content of card" />, div);
+  
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
---
+++ src/List.test.js
@@ -0,0 +1,13 @@
+// import React from 'react';
+// import ReactDOM from '.react-dom';
+// import Card from '/.List';
+
+// describe('<List />', () =>{
+//   it('renders without crashing', ()=> {
+//     const div = document.createElement('div');
+  
+//     ReactDOM.render(<Card key="1" header="List1" cards="[card1, card2, card3]" />, div);
+  
+//     ReactDOM.unmountComponentAtNode(div);
+//   });
+// });
